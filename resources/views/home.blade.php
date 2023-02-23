6e93fc42f3c90bf0863f9420195b526fa9d1d7f2
NicolaPlacentino <nyko0921@gmail.com>
2023-02-23 18:03:38 +0100

    completed exercise

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,6 +9,30 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    
+    @include('includes.header')
+    <main>
+        <div class="jumbo">
+            <img src="{{asset('images/jumbotron.jpg')}}" alt="jumbotron">
+        </div>
+        <div class="container">
+            <h2>Current series</h2>
+            <div class="card-section">
+                @foreach ($comics as $comic)
+                <div class="card">
+                    <figure>
+                        <img src="{{$comic['thumb']}}" alt="{{$comic['series']}}">
+                        <figcaption>{{$comic['series']}}</figcaption>
+                    </figure>
+                </div>
+                @endforeach
+
+            </div>
+            <div class="text-center">
+                <button>Load More</button>
+            </div>
+        </div>
+    </main>
+    @include('includes.footer')
+
 </body>
 </html>
